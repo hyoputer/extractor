b38266e2eb75bac990d875e1461f19932925ea89
unknown <15gyals@snu.ac.kr>
2020-04-29 13:50:57 +0900

    labeling.bat

---
+++ labeling.bat
@@ -0,0 +1,26 @@
+
+setlocal enabledelayedexpansion
+dir /b /ad > list
+
+mkdir files
+set FILESPATH=%cd%\files
+set numfiles=0
+
+for /f "delims=" %%i in (list) do (
+for /f "delims=" %%j in (checklist.txt) do (
+cd %%i
+dir /s /b | find "%%j" > check
+set numfiles=0
+for /f "delims=" %%k in (check) do (
+set /a numfiles+=1
+cd %FILESPATH%
+copy %%k %FILESPATH%
+ren %%j "%%i!numfiles!-%%j"
+)
+@cd "%cd%\%%i"
+@del check
+@cd %cd%
+))
+del list
+pause
+endlocal
