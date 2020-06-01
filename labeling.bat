6d0bce4dc5eed93f637be42b45a887384d123e62
unknown <15gyals@snu.ac.kr>
2020-06-01 16:50:04 +0900

    indent

--- labeling.bat
+++ labeling.bat
@@ -7,20 +7,22 @@ set FILESPATH=%cd%\files
 set numfiles=0
 
 for /f "delims=" %%i in (list) do (
-for /f "delims=" %%j in (checklist.txt) do (
-cd %%i
-dir /s /b | find /i "%%j" > check
-set numfiles=0
-for /f "delims=" %%k in (check) do (
-set /a numfiles+=1
-cd %FILESPATH%
-copy %%k %FILESPATH%
-ren %%j "%%i!numfiles!-%%j"
+  for /f "delims=" %%j in (checklist.txt) do (
+    cd %%i
+    dir /s /b | find /i "%%j" > dircheck
+    dir /s
+    set numfiles=0
+    for /f "delims=" %%k in (check) do (
+      set /a numfiles+=1
+      cd %FILESPATH%
+      copy %%k %FILESPATH%
+      ren %%j "%%i!numfiles!-%%j"
+    )
+    @cd "%cd%\%%i"
+    @del check
+    @cd %cd%
+  )
 )
-@cd "%cd%\%%i"
-@del check
-@cd %cd%
-))
 del list
 pause
 endlocal
