16b29c8b5a12d0302c62991f463dd1a95962fcb0
unknown <15gyals@snu.ac.kr>
2020-06-01 17:11:26 +0900

    txt files

--- labeling.bat
+++ labeling.bat
@@ -1,27 +1,27 @@
 
 setlocal enabledelayedexpansion
-dir /b /ad > list
+dir /b /ad > list.txt
 
 mkdir files
 set FILESPATH=%cd%\files
 set numfiles=0
 
-for /f "delims=" %%i in (list) do (
+for /f "delims=" %%i in (list.txt) do (
   for /f "delims=" %%j in (checklist.txt) do (
     cd %%i
-    dir /s /b | find /i "%%j" > check
+    dir /s /b | find /i "%%j" > check.txt
     set numfiles=0
-    for /f "delims=" %%k in (check) do (
+    for /f "delims=" %%k in (check.txt) do (
       set /a numfiles+=1
       cd %FILESPATH%
       copy %%k %FILESPATH%
       ren %%j "%%i!numfiles!-%%j"
     )
     cd "%cd%\%%i"
-    del check
+    del check.txt
     cd %cd%
   )
 )
-del list
+del list.txt
 pause
 endlocal
