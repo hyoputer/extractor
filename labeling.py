2f2752345d8532de4c5d418e174875b7f5d45da9
unknown <15gyals@snu.ac.kr>
2020-06-02 15:31:44 +0900

    python version

---
+++ labeling.py
@@ -0,0 +1,40 @@
+import os
+import shutil
+
+os.system('mkdir files')
+
+checklist = open("checklist.txt", 'r')
+cklist = checklist.readlines()
+
+filelist = []
+extlist = []
+
+for ck in cklist:
+  if (ck[0] == '.'):
+    extlist.append(ck.rstrip())
+  else:
+    filelist.append(ck.rstrip())
+
+filespath = os.path.join(os.getcwd(), "files")
+
+tmp = os.listdir(os.getcwd())
+for dirname in tmp:
+  dirpath = os.path.join(os.getcwd(), dirname)
+  if os.path.isdir(dirpath) and dirpath != filespath:
+    idx = 1
+    for (path, dir, files) in os.walk(dirpath):
+      for filename in files:
+        copyflag = False
+        fullpath = os.path.join(path, filename)
+        for ckname in filelist:
+          if ckname == filename:
+            copyflag = True
+        ext = os.path.splitext(filename)[-1]
+        for extname in extlist:
+          if extname == ext:
+            copyflag = True
+
+        if copyflag:
+          shutil.copy(fullpath, filespath)
+          os.rename(os.path.join(filespath, filename), os.path.join(filespath, dirname + str(idx) + '-' + filename))
+          idx+=1
