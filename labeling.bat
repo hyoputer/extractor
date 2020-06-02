200fcbd0a63605b3595bfb31ea92005a7e25737b
unknown <15gyals@snu.ac.kr>
2020-06-02 10:51:47 +0900

    trim whitespace

--- labeling.bat
+++ labeling.bat
@@ -4,23 +4,28 @@ dir /b /ad > list.txt
 
 mkdir files
 set FILESPATH=%cd%\files
-set numfiles=0
 
 for /f "delims=" %%i in (list.txt) do (
-  for /f "delims=" %%j in (checklist.txt) do (
+  for /f "delims= " %%j in (checklist.txt) do (
     cd %%i
-    dir /s /b | find /i "%%j" > check.txt
-    set numfiles=0
-    for /f "delims=" %%k in (check.txt) do (
-      set /a numfiles+=1
-      cd %FILESPATH%
-      copy %%k %FILESPATH%
-      ren %%~nk%%~xk "%%i!numfiles!-%%~nk%%~xk"
+    if exist check (
+      dir /s /b | find /i "%%j" >> check
+    ) else (
+      dir /s /b | find /i "%%j" > check
     )
-    cd "%cd%\%%i"
-    del check.txt
-    cd %cd%
   )
+  set numfiles=0
+  for /f "delims=" %%k in (check) do (
+    cd %FILESPATH%
+    copy %%k %FILESPATH%
+    if exist %%~nk%%~xk (
+      set /a numfiles+=1
+    )
+    ren %%~nk%%~xk "%%i!numfiles!-%%~nk%%~xk"
+  )
+  cd "%cd%\%%i"
+  del check
+  cd %cd%
 )
 del list.txt
 pause
