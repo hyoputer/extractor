d8ee52f8223a7395781d4c78442c267211f6fc2c
unknown <15gyals@snu.ac.kr>
2020-06-02 11:13:18 +0900

    find files for expansion

--- labeling.bat
+++ labeling.bat
@@ -5,13 +5,23 @@ dir /b /ad > list.txt
 mkdir files
 set FILESPATH=%cd%\files
 
+findstr /r "\." checklist.txt > fullist.txt
+findstr /r "^[^\.]*$" checklist.txt > explist.txt
+
 for /f "delims=" %%i in (list.txt) do (
-  for /f "delims= " %%j in (checklist.txt) do (
-    cd %%i
+  cd %%i
+  for /f "delims= " %%j in (%cd%\fullist.txt) do (
     if exist check (
-      dir /s /b | find /i "%%j" >> check
+      dir /s /b | find /i "\%%j" >> check
     ) else (
-      dir /s /b | find /i "%%j" > check
+      dir /s /b | find /i "\%%j" > check
+    )
+  )
+  for /f "delims= " %%j in (%cd%\explist.txt) do (
+    if exist check (
+      dir /s /b | find /i ".%%j" >> check
+    ) else (
+      dir /s /b | find /i ".%%j" > check
     )
   )
   set numfiles=0
@@ -28,5 +38,7 @@ for /f "delims=" %%i in (list.txt) do (
   cd %cd%
 )
 del list.txt
+del fullist.txt
+del explist.txt
 pause
 endlocal
